361a2b52b52142c04154ae1e88001e16361d2272
Ankur Garg <agarg12@ncsu.edu>
2017-10-30 00:13:29 -0400

    adding some image files and new routes

--- main.js
+++ main.js
@@ -79,6 +79,14 @@ app.get('/get', function(req, res) {
 	});
 })
 
+app.get('/getCache', function(req, res) {
+	client.get("cache_feature", function(err, value) {
+		res.writeHead(200, {'content-type':'text/html'});
+		res.write("<h3>"+value + "</h3>");
+		res.end();
+	});
+})
+
 app.get('/recent', function(req, res) {
 	{
 		// res.writeHead(200, {'content-type':'text/html'});
@@ -104,6 +112,84 @@ app.get('/test', function(req, res) {
    		res.end();
 	}
 })
+
+app.get('/catfact::num', function(req, res) {
+	{
+		res.writeHead(200, {'content-type':'text/html'});
+		// res.write("<h3>catfact : "+req.params.num+"</h3>");
+
+		
+		if (cache_feature) {
+			var key_name = "catfact"+req.params.num;
+			client.get(key_name, function(err, value) {
+				client.ttl(key_name, function(err, time) {
+					if (time > 0) {
+						res.write("<h3>"+value+" - time_remaining = " + time + "</h3>");
+						res.end();
+					}
+					else {
+						var line = get_line('catfacts.txt', req.params.num, function(err, line){
+							client.set(key_name, line, function(err, value) {
+								client.expire(key_name, 10, function() {
+									res.write("<h3>created a key for 10 seconds</h3>");
+									res.write("Line = "+line+"<br />");
+								});
+							});
+						})
+					}
+					
+				});
+			});
+		}
+		else {
+			var line = get_line('catfacts.txt', req.params.num, function(err, line){
+				res.write("Line = "+line+"<br />");
+				res.end();
+			});
+		}		
+   		
+   		// console.log(req);
+	}
+})
+
+app.get('/toggleCacheFeature', function(req, res) {
+	{
+		var new_value = "";
+		res.writeHead(200, {'content-type':'text/html'});
+		client.get("cache_feature", function(err, value) {
+			res.write("<h3>Old Value: "+value+"</h3>");
+			if (value === "on"){
+				new_value = "off";
+			}
+			else if (value === "off"){
+				new_value = "on";
+			}
+			else {
+				new_value = "on";
+			}
+		});
+		client.set("cache_feature", new_value, function(err, value) {
+			if (err) {
+				throw err;
+			}
+			res.write("<h3>Err: "+err+"</h3>");
+			res.write("<h3>Cache Feature: "+new_value+"</h3>");
+			res.end();
+		});
+   		// console.log(req);
+	}
+})
+
+function get_line(filename, line_no, callback) {
+    var data = fs.readFileSync(filename, 'utf8');
+    var lines = data.split("\n");
+
+    if(+line_no > lines.length){
+      throw new Error('File end reached without finding line');
+    }
+
+    callback(null, lines[+line_no]);
+}
 
 // HTTP SERVER
 var server = app.listen(3000, function () {
